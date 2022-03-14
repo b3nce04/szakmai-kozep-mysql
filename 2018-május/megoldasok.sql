305945d7c6cd6c203bcb34ab8455d22392c5cea6
Bence Hagymási <hagymasibence@hotmail.com>
2022-03-14 20:49:52 +0100

    commit

---
+++ 2018-május/megoldasok.sql
@@ -0,0 +1,35 @@
+﻿A feladatok megoldására elkészített SQL parancsokat illessze be a feladat sorszáma után!
+
+
+1. feladat:
+CREATE DATABASE maratonvalto
+DEFAULT CHARACTER SET utf8
+COLLATE utf8_hungarian_ci;
+
+3. feladat:
+INSERT INTO `eredmenyek` VALUES (1044,4,15765)
+
+4. feladat:
+SELECT fnev as futo, szulev
+FROM `futok` 
+WHERE NOT ffi
+ORDER BY fnev ASC;
+
+5. feladat:
+SELECT fnev as futo, (2016-szulev) as kor
+FROM `futok` 
+WHERE (2016-szulev) >= 42
+ORDER BY szulev ASC, szulho ASC;
+
+6. feladat:
+SELECT futok.fnev as futo, eredmenyek.ido
+FROM `eredmenyek` INNER JOIN futok ON eredmenyek.futo = futok.fid
+WHERE futok.ffi
+ORDER BY eredmenyek.ido ASC
+LIMIT 10;
+
+7. feladat:
+SELECT futok.csapat, SUM(eredmenyek.ido) as csapatido
+FROM `futok` INNER JOIN eredmenyek ON eredmenyek.futo = futok.fid
+GROUP BY futok.csapat
+ORDER BY csapatido ASC;
---
+++ 2018-október/megoldasok.sql
@@ -0,0 +1,38 @@
+﻿A feladatok megoldására elkészített SQL parancsokat illessze be a feladat sorszáma után!
+
+
+1. feladat:
+CREATE DATABASE teke
+DEFAULT CHARACTER SET utf8
+COLLATE utf8_hungarian_ci;
+
+3. feladat:
+SELECT nev
+FROM `versenyzok` 
+WHERE korcsop = 'A'
+ORDER BY nev asc;
+
+4. feladat:
+SELECT DISTINCT versenyzo
+FROM `eredmenyek`
+WHERE ures > 0;
+
+5. feladat:
+SELECT versenyzok.nev, AVG(eredmenyek.tarolas) as atlagpont
+FROM `eredmenyek` INNER JOIN versenyzok ON versenyzok.rajtszam = eredmenyek.versenyzo
+GROUP BY versenyzok.rajtszam
+ORDER BY atlagpont DESC;
+
+6. feladat:
+SELECT egyesuletek.nev
+FROM `egyesuletek` INNER JOIN versenyzok ON versenyzok.egyid = egyesuletek.id
+GROUP BY egyesuletek.id
+ORDER BY count(versenyzok.rajtszam) DESC
+LIMIT 1;
+
+7. feladat:
+SELECT versenyzok.nev, SUM(eredmenyek.teli+eredmenyek.tarolas) as eredmeny, SUM(eredmenyek.tarolas), SUM(eredmenyek.ures)
+FROM `versenyzok` INNER JOIN eredmenyek ON versenyzok.rajtszam = eredmenyek.versenyzo
+WHERE versenyzok.korcsop = 'B'
+GROUP BY versenyzok.rajtszam
+ORDER BY eredmeny DESC, eredmenyek.tarolas DESC, eredmenyek.ures ASC;
